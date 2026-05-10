ac43399fbde1b813d52babc355c8854df911745d
L-Criss <linoc.joachin@gmail.com>
2026-05-10 18:41:43 -0500

    Implementación de Clases Abastractas

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,7 @@
 import acuario.Acuario
+import acuario.PezPayaso
 import acuario.TanqueTorre
+import acuario.Tiburon
 
 
 fun construirAcuario(){
@@ -13,6 +15,16 @@ fun construirAcuario(){
 
 }
 
+fun crearPeces() {
+
+    val tiburon = Tiburon()
+    val pezPayaso = PezPayaso()
+
+    println("Color del tiburon: ${tiburon.color}")
+    println("Color del pez payaso: ${pezPayaso.color}")
+}
+
 fun main(){
-    construirAcuario()
+    //construirAcuario()
+    crearPeces()
 }
---
+++ src/acuario/PecesAcuario.kt
@@ -0,0 +1,16 @@
+package acuario
+
+abstract class Pez {
+
+    abstract val color: String
+}
+
+class Tiburon : Pez() {
+
+    override val color: String = "gris"
+}
+
+class PezPayaso : Pez() {
+
+    override val color: String = "dorado"
+}
